1e18739a0f2d5872e7dcb7c8a75c27b14c595167
Josip Rezić <josip.rezic@hotmail.com>
2019-08-15 18:59:07 +0200

    Initial Commit

---
+++ GymAssistant/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GymAssistant
+//
+//  Created by Korisnik on 15/08/2019.
+//  Copyright © 2019 Josip Rezic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
